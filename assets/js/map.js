312cdb0fd306f196e99471198711f78e33e1ee41
Najib El Guertit <najib.elguertit@ELGUERTIT-MAC.lan>
2023-02-11 02:02:36 +0100

    fixing issues related to map

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,7 +1,6 @@
 var map = L.map('map').setView([45.4991,9.1328], 12);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
 maxZoom: 18,
 id: 'mapbox/streets-v11',
 tileSize: 512,
@@ -10,30 +9,76 @@ accessToken: 'pk.eyJ1IjoibWlrZWJvc3MwOSIsImEiOiJja3k5MnY3ZTcwMmRnMm5venVoNGV3eWp
 }).addTo(map);
 
 
-/* 
+  var dataMarker =[
+    {
+      "lat": 45.50145,
+      "lng": 9.12264,
+      "name": "Cindy",
+      "phone": 3511015238
+    },
+    {
+      "lat": 45.48756,
+      "lng": 9.18524,
+      "name": "Gaia",
+      "phone": 3939319999
+    },
+    {
+      "lat": 45.51051,
+      "lng": 9.08309,
+      "name": "Sabrina",
+      "phone": 3511039481
+    },
+    {
+      "lat": 45.49610,
+      "lng": 9.17581,
+      "name": "Laura",
+      "phone": 3272835966
+    }
+  ]
 
-var circle = L.circle([45.48756, 9.18524], {
-color: 'red',
-fillColor: '#f03',
-fillOpacity: 0.5,
-radius: 500
-}).addTo(map);
+  dataMarker.forEach(person => {
+      let content_marker = `<b>${person.name}</b><br>${person.phone}`;
+      let mark=L.marker([person.lat, person.lng])
+      .bindPopup(content_marker)
+      .addTo(map);
+    });
+ 
 
-circle.bindPopup("<b>Gaia</b><br>3939319999.");
- */
-var marker1 = L.marker([45.48756,9.18524]).addTo(map);
-marker1.bindPopup("<b>Gaia</b><br>3939319999").openPopup();
 
-//delivery
+    var datacircles =[
+      {
+        "lat": 45.47580,
+        "lng": 9.15403,
+        "name": "Electronics",
+        "fillcolor": '#ac6e0e'
+      },
+      {
+        "lat": 45.49670,
+        "lng": 9.11008,
+        "name": "Scarpe",
+        "fillcolor": '#f50692'
+      }
+    ]
+    datacircles.forEach(point=>{
+      let content_circle =`<b>${point.name}</b>`;
+      let cr=L.circle([point.lat,point.lng],{
+        color: 'red',
+        fillColor: point.fillcolor,
+        fillOpacity: 0.5,
+        radius: 500
+      }).bindPopup(content_circle).addTo(map);
+    });
 
- var polygon = L.polygon([
+//delivery area
+
+/*  var polygon = L.polygon([
 [45.5288,9.0396],
 [45.4228,9.0643],
 [45.3785,9.1655],
 [45.4180,9.2701],
 [45.4908,9.2934],
 [45.5593,9.2089]
-]).addTo(map);
+]).addTo(map); */
  
 /* var polygon = L.polygon([
     [45.5119, 9.0850],
